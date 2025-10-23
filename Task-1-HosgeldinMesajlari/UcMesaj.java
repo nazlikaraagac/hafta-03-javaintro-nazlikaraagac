9d26e4c8395b89ad93d1937c2a22664039b79070
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 17:48:29 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Bilgisayar Bilimine Hoşgeldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
