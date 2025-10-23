a5b2b2d90a0ecfadc5752aa20d10f92b78585e1c
nazlikaraagac <250541105@firat.edu.tr>
2025-10-24 00:00:57 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("N     N    A     ZZZZZZ    L        I");
+        System.out.println("NN    N   A A         Z    L        I");
+        System.out.println("N N   N  A   A       Z     L        I");
+        System.out.println("N  N  N AAAAAAA     Z      L        I");
+        System.out.println("N   N N A     A    Z       L        I");
+        System.out.println("N    NN A     A   Z        L        I");
+        System.out.println("N     N A     A  ZZZZZZZ   LLLLLLLL I");
+    }
+}
