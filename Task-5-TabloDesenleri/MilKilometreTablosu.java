7375c092eb6d347bbd5893b57c9cd291f7900582
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 21:02:26 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,21 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        System.out.println("Mil   Kilometre");
+        System.out.println("----------------");
+
+        int mil = 1;
+        System.out.println(mil + "     " + (mil * 1.609));
+
+        mil = 5;
+        System.out.println(mil + "     " + (mil * 1.609));
+
+        mil = 10;
+        System.out.println(mil + "     " + (mil * 1.609));
+
+        mil = 20;
+        System.out.println(mil + "     " + (mil * 1.609));
+
+        mil = 50;
+        System.out.println(mil + "     " + (mil * 1.609));
+    }
+}
