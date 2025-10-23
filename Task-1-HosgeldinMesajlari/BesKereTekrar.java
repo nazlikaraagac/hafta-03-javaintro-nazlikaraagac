6a5e8abff3dbc3dc136b9a76d4000ae878471881
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 17:42:20 +0300

    Create BesKereTekrar.java to print welcome message

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+    }
+}
