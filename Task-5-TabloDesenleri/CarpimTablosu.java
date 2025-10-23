e1109857b42d332b44f9896405d5ee8e30860678
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 20:38:21 +0300

    Add multiplication table for the number 2

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,19 @@
+public class CarpimTablosu{
+    public static void main (String[]args){
+
+        int sayi = 2;
+
+        System.out.println("------- Carpim Tablosu-------");
+        System.out.println(sayi + " x 1 = " + (sayi * 1));
+        System.out.println(sayi + " x 2 = " + (sayi * 2));
+        System.out.println(sayi + " x 3 = " + (sayi * 3));
+        System.out.println(sayi + " x 4 = " + (sayi * 4));
+        System.out.println(sayi + " x 5 = " + (sayi * 5));
+        System.out.println(sayi + " x 6 = " + (sayi * 6));
+        System.out.println(sayi + " x 7 = " + (sayi * 7));
+        System.out.println(sayi + " x 8 = " + (sayi * 8));
+        System.out.println(sayi + " x 9 = " + (sayi * 9));
+        System.out.println(sayi + " x 10 = " + (sayi * 10));
+        System.out.println("-----------------------------");
+        }
+    }
