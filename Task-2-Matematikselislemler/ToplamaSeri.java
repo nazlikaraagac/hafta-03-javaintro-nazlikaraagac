d0eba7dcab0eba84993fc31d6caf970643e52df0
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 18:16:07 +0300

    Implement sum calculation from 1 to 9
    Calculate the sum of numbers from 1 to 9 and print the result.

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,8 @@
+public class ToplamaSeri {
+    public static void main(String[] args) {
+        // Sonucu hesapla
+        int sonuc = 1+2+3+4+5+6+7+8+9;
+ // Ekrana yazdir
+ System.out.println("1+2+...+9 = " + sonuc);
+    }
+}
