be3b6ce9465889731158acbb2c01433f3a061e72
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 20:53:35 +0300

    Implement power table for numbers 1 to 5

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,21 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args) {
+        System.out.println("a   a^2   a^3");
+        System.out.println("----------------");
+
+        int a1 = 1;
+        System.out.println(a1 + "   " + (a1 * a1) + "    " + (a1 * a1 * a1));
+
+        a1 = 2;
+        System.out.println(a1 + "   " + (a1 * a1) + "    " + (a1 * a1 * a1));
+
+        a1 = 3;
+        System.out.println(a1 + "   " + (a1 * a1) + "    " + (a1 * a1 * a1));
+
+        a1 = 4;
+        System.out.println(a1 + "   " + (a1 * a1) + "    " + (a1 * a1 * a1));
+
+        a1 = 5;
+        System.out.println(a1 + "   " + (a1 * a1) + "   " + (a1 * a1 * a1));
+    }
+}
