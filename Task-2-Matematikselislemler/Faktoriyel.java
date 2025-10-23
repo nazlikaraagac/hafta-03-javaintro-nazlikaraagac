f31ca844cd9f00d5e864e2dba2ebedba36a2f780
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 18:58:07 +0300

    Add Faktoriyel class to calculate factorial of 5

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,7 @@
+public class Faktoriyel {
+    public static void main (String [] args ){
+        int sonuc = 5*4*3*2*1;
+       System.out.println("5!  = " + sonuc);    
+        }
+        
+    }
