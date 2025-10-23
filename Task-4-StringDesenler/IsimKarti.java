034253dd9e198d2019c01375298e0ff8621ba598
nazlikaraagac <250541105@firat.edu.tr>
2025-10-23 19:41:19 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+ public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Nazli";
+        String soyad = "Karaagac";
+        String ogrNo = "250541105";
+        String bolum = "Yazilim Muhendisligi";
+        System.out.println("===================");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("===================");
+        System.out.println("Ad : " + ad);
+        System.out.println("Soyad : " + soyad);
+        System.out.println("No : " + ogrNo);
+        System.out.println("Bolum : " + bolum);
+        System.out.println("===================");
+    }
+}
